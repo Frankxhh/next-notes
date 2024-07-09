4b41f2e33f294fde55829c29f444f31cd5e8331d
zhangxinhui <zhang.xinhui@cesgroup.com.cn>
2024-07-09 11:17:58 +0800

    feat: 新增公共sidebar组件-home样式简单修改;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import SideBar from "@/components/SideBar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div className={'flex gap-8'}>
+          <SideBar/>
+          <section className={'flex-1'}>{children}</section>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,9 @@
+'use client'
+export default function SideBar() {
+
+    return (
+        <div className={'p-4 bg-white shadow-lg shadow-gray-300 min-w-[300px] text-center'}>
+            <h1 className="text-4xl font-bold ">hela notes</h1>
+        </div>
+    )
+}
