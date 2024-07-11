95a6d3f89907cf685aa328dd0df60dc1037fde01
zhangxinhui <zhang.xinhui@cesgroup.com.cn>
2024-07-11 16:40:24 +0800

    feat: 增加prettier;

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tabWidth: 4,
+    semi: true,
+    printWidth: 120,
+    singleQuote: true,
+    quoteProps: 'consistent',
+    htmlWhitespaceSensitivity: 'strict',
+    vueIndentScriptAndStyle: true,
+    plugins: ['prettier-plugin-tailwindcss'],
+};
